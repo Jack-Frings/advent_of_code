704a4bed1db1a0287cd097560209fbba2ce8500d
jack-frings <jackdfrings@protonmail.com>
2025-12-01 22:49:24 -0800

    stayed up way too late optimizing; regex is the way

--- 2025/02/id.py
+++ 2025/02/id.py
@@ -1,3 +1,4 @@
+import re
 import time 
 start = time.time()
 
@@ -10,10 +11,12 @@ def part_one():
     id_bounds = line.split(",")
     sum = 0
 
+    regex = re.compile(r'^(\d*)\1$')
+
     for id_bound in id_bounds:
         from_bound, to_bound = id_bound.split("-")
         for id in range(int(from_bound), int(to_bound) + 1):
-            if isInvalid(id):
+            if regex.match(str(id)):
                 sum += id 
 
     return sum
@@ -27,44 +30,15 @@ def part_two():
     id_bounds = line.split(",")
     sum = 0
 
+    regex = re.compile(r'^(\d*)\1+$')
+
     for id_bound in id_bounds:
         from_bound, to_bound = id_bound.split("-")
         for id in range(int(from_bound), int(to_bound) + 1):
-            if isInvalid2(id):
+            if regex.match(str(id)):
                 sum += id 
 
     return sum
-
-def isInvalid(id):
-    id = str(id)
-    if len(id) % 2 == 1:
-        return False 
-
-    halfway = int(len(id) / 2)
-
-    if id[:halfway] == id[halfway:]:
-        return True 
-
-    return False
-
-def isInvalid2(id):
-    id = str(id)
-    length = len(id)
-    halfway = int(len(id) / 2)
-
-    for num in range(1, halfway+1):
-        if length % num == 0:
-            invalid = True 
-            phrase = id[:num] 
-            for i in range(num, length-num+1, num):
-                if id[i:i+num] != phrase:
-                    invalid = False
-                    break 
-
-            if invalid:
-                return True 
-
-    return False 
 
 if __name__ == "__main__":
     print(f"Part One: {part_one()}")
