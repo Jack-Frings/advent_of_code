0d22bc27f53654e25f26f1d89ff0a2e602ddfe1f
jack-frings <jackdfrings@protonmail.com>
2025-12-07 23:02:03 -0800

    slight optimization ; going to sleep now

--- 2025/08/junctions.py
+++ 2025/08/junctions.py
@@ -1,18 +1,19 @@
 import time, math, sys
 start = time.time()
 
-def part_one():
+def getDistances():
     with open("8.txt") as file:
         lines = list(file.readlines())
 
+    global points
     points = set()
 
     for line in lines:
-        line = line.strip("\n") 
-        x, y, z = line.split(",")
+        x, y, z = line.strip("\n").split(",")
         points.add((int(x), int(y), int(z)))
 
     # Memorize distance values to avoid needing to recalc
+    global distances
     distances = {}
     for point_a in points:
         for point_b in points:
@@ -21,9 +22,17 @@ def part_one():
                 distances[(point_a, point_b)] = getDistance(point_a, point_b)
 
     # Sort the dictionary by the distance from least to greatest
-    distances = dict(sorted(distances.items(), key=lambda item: item[1]))
-    keys = list(distances.keys())
+    distances = dict(sorted(distances.items(), key=lambda dist: dist[1]))
 
+def getDistance(point_a, point_b):
+    x1, y1, z1 = point_a
+    x2, y2, z2 = point_b 
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
+
+def part_one():
+    global points 
+    global distances
+    keys = list(distances.keys())
     circuits = [[point] for point in points] 
 
     for i in range(1000):
@@ -50,35 +59,11 @@ def part_one():
     lengths = sorted([len(circuit) for circuit in circuits], reverse=True)
     return lengths[0] * lengths[1] * lengths[2]
 
-def getDistance(point_a, point_b):
-    x1, y1, z1 = point_a
-    x2, y2, z2 = point_b 
-    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
-
         
 def part_two():
-    with open("8.txt") as file:
-        lines = list(file.readlines())
-
-    points = set()
-
-    for line in lines:
-        line = line.strip("\n") 
-        x, y, z = line.split(",")
-        points.add((int(x), int(y), int(z)))
-
-    # Memorize distance values to avoid needing to recalc
-    distances = {}
-    for point_a in points:
-        for point_b in points:
-            if point_a == point_b: continue 
-            if not ((point_a, point_b) in distances.keys()) and not ((point_b, point_a) in distances.keys()):
-                distances[(point_a, point_b)] = getDistance(point_a, point_b)
-
-    # Sort the dictionary by the distance from least to greatest
-    distances = dict(sorted(distances.items(), key=lambda item: item[1]))
+    global points 
+    global distances
     keys = list(distances.keys())
-
     circuits = [[point] for point in points] 
 
     i = 0
@@ -109,7 +94,9 @@ def part_two():
 
     return x1*x2
 
+
 if __name__ == "__main__":
+    getDistances()
     print(f"Part One: {part_one()}")
     print(f"Part Two: {part_two()}")
     print(f"Execution Time: {time.time() - start}")
